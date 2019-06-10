8e11ad8f9e469f2a08811e77073bcc33083f0bbe
teichx <teichx@hotmail.com>
2019-06-10 01:52:58 -0300

    Aded sensor with smoothscroll recyclerView

--- app/src/main/java/com/example/android/noqfoodprototype/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/noqfoodprototype/Activities/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.noqfoodprototype.Activities;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
 import android.view.Menu;
@@ -12,13 +17,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
 import Adapters.CardapioAdapter;
 import Adapters.EstabilishmentItemAdapter;
 import Utils.StartSnapHelper;
 import com.example.android.noqfoodprototype.GetList;
 import com.example.android.noqfoodprototype.R;
 import com.google.firebase.auth.FirebaseAuth;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements CardapioAdapter.ItemClickListener {
 
@@ -27,20 +32,37 @@ public class MainActivity extends AppCompatActivity implements CardapioAdapter.I
 
     private LinearLayoutManager horizontalLayoutManager;
     private LinearLayoutManager verticalLayout;
+    private RecyclerView.SmoothScroller smoothScroller;
+
+
+    private SensorManager sensorManager;
+    private Sensor rotation;
+    private SensorEventListener rotationListener;
+
+    private int pos = 0;
+    private int maxPos;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Actibity que será usada
         setContentView(R.layout.activity_estabilhments_itens);
 
+        smoothScroller = new LinearSmoothScroller(this) {
+            @Override protected int getVerticalSnapPreference() {
+                return LinearSmoothScroller.SNAP_TO_START;
+            }
+        };
 
         //Lista de estabelecimentos
         //Set RecyclerView da lista de estabelecimentos
-        RecyclerView recyclerView = findViewById(R.id.estabilishmentsList);
+        ArrayList estabelecimentos =  GetList.getEstabelecimentos();
+        maxPos = estabelecimentos.size() -1;
+
+        final RecyclerView recyclerView = findViewById(R.id.estabilishmentsList);
         horizontalLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager( horizontalLayoutManager );
-        adapter = new EstabilishmentItemAdapter(this, GetList.getEstabelecimentos() );
+        adapter = new EstabilishmentItemAdapter(this, estabelecimentos);
         //Seta o scrolllistener do RecyclerView
         recyclerView.addOnScrollListener(onScrollListener);
         recyclerView.setAdapter(adapter);
@@ -49,11 +71,58 @@ public class MainActivity extends AppCompatActivity implements CardapioAdapter.I
         SnapHelper startSnapHelper = new StartSnapHelper();
         startSnapHelper.attachToRecyclerView( recyclerView );
 
-
+        //Seta os itens do cardápio do primeiro estabelecimento da lista
         RecyclerView cardapioRecycler = findViewById(R.id.cardapioList);
         verticalLayout = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
         cardapioRecycler.setLayoutManager(verticalLayout);
         setCardapio( 0 );
+
+        //Sensor
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        rotation = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        rotationListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                //Esquerda
+                if ( event.values[2] > 0.25f ){
+                    if ( pos >= 1 ){
+                        pos -= 1;
+
+                        smoothScroller.setTargetPosition( pos );
+                        horizontalLayoutManager.startSmoothScroll(smoothScroller);
+                    }
+                }
+                //Direita
+                else if( event.values[2] < 0.15f ){
+                    if ( pos < maxPos ){
+                        pos += 1;
+
+                        smoothScroller.setTargetPosition( pos );
+                        horizontalLayoutManager.startSmoothScroll(smoothScroller);
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+    }
+
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(rotationListener, rotation, sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(rotationListener);
     }
 
     @Override
